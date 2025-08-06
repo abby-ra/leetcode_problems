4d295e5711417cb80cdf3c3ddff299c3331cd59f
abby-ra <abbynayaraj@gmail.com>
2025-08-06 10:16:55 +0530

    1 updated

--- 1.two_sums.py
+++ 1.two_sums.py
@@ -1,9 +1,6 @@
 class Solution:
     def twoSum(self, nums, target):
-        num_map = {}
-        for i, num in enumerate(nums):
-            complement = target - num
-            if complement in num_map:
-                return [num_map[complement], i]
-            num_map[num] = i
-        return []
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i+j]
